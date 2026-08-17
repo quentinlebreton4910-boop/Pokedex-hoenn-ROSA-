83f61591bc1396bd8cc1b1bd0339d8ad00240782
quentinlebreton4910-boop <quentin.lebreton4910@gmail.com>
2026-08-17 23:57:24 +0200

    Update accueil.js

--- accueil.js
+++ accueil.js
@@ -124,7 +124,7 @@ function createPokemonBackground(region = "all") {
    */
 
   const count = Math.min(
-    window.innerWidth < 600 ? 35 : 50,
+    window.innerWidth < 600 ? 18 : 30,
     shuffled.length
   );
 
